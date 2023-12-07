f7112b708ac63ec41e148e1cf7b013e92e0d384a
Ebrheem Abdel Aziz <ebraheemzezo011@gmail.com>
2023-12-07 18:19:11 +0200

    update buy skin api

--- routes/Skins.js
+++ routes/Skins.js
@@ -4,7 +4,7 @@ const util = require("util");
 const authorized = require("../middleware/authorize");
 
 // unlocked skins list
-router.get("/unlocked",authorized, async (req, res) => {
+router.get("/unlocked", authorized, async (req, res) => {
   try {
     const query = util.promisify(conn.query).bind(conn); // for multiple query
     const userID = res.locals.user.id;
@@ -40,36 +40,54 @@ router.get("/locked", authorized, async (req, res) => {
 });
 // user can buy a new skin
 router.post("/buy/:skinid", authorized, async (req, res) => {
-try {
+  try {
     const query = util.promisify(conn.query).bind(conn); // for multiple query
     const userID = res.locals.user.id;
-    const skinId = req.params.skinid
+    const skinId = req.params.skinid;
     const skinsData = await query(
       "SELECT * FROM skins WHERE NOT EXISTS ( SELECT 1 FROM usersskins WHERE skins.id = usersskins.skinId and usersskins.userId = ? ) and skins.id = ?",
       [userID, skinId]
     );
     if (skinsData.length == 0) {
+      res.status(400).json({
+        errors: [
+          {
+            msg: "Invaled skinId",
+          },
+        ],
+      });
+    } else {
+      const userCoins = await query("select coins from users where id = ?", [
+        userID,
+      ]);
+      if (userCoins[0].coins < skinsData[0].price) {
         res.status(400).json({
           errors: [
             {
-              msg: "Invaled skinId",
+              msg: "you don't have enough coins",
             },
           ],
         });
-    }else{
+      }else{
         const buySkin = {
           userId: userID,
-          skinId:skinId
+          skinId: skinId,
         };
+        const newCoins = {
+          coins: userCoins[0].coins - skinsData[0].price,
+        };
+        await query("update users set ? where id = ?", [newCoins, userID]);
         await query("insert into usersskins set ?", buySkin);
         res.status(200).json({
           msg: "skin is now yours",
         });
+      }
+      
     }
-} catch (error) {
+  } catch (error) {
     res.status(500).json({
       error: error,
     });
-}
+  }
 });
 module.exports = router;
