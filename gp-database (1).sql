5367a9f48f9bfb7830cdbdcd97a32769a6d80b4d
Ebraheem Abdel Aziz <ebraheemzezo011@gmail.com>
2024-03-08 16:01:02 +0200

    adding new database and postman collection

--- gp-database (1).sql
+++ gp-database (1).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 07, 2024 at 01:24 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Host: 127.0.0.1:3307
+-- Generation Time: Mar 07, 2024 at 02:27 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `skins` (
   `modelUrl` varchar(255) NOT NULL,
   `positionPlane` varchar(255) NOT NULL,
   `price` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `skins`
@@ -44,7 +44,10 @@ CREATE TABLE `skins` (
 INSERT INTO `skins` (`id`, `name`, `imageUrl`, `scale`, `modelUrl`, `positionPlane`, `price`) VALUES
 (9, 'Retro', '1709761143898.png', '{\"x\":0.025,\"y\":0.025,\"z\":0.025}', 'models/plane5/skin.glb', '{\"x\":0,\"y\":1,\"z\":0}', 500),
 (10, 'Ghost', '1709761249214.png', '{\"x\":0.2,\"y\":0.2,\"z\":0.2}', 'models/plane2/skin.glb', '{\"x\":0,\"y\":1,\"z\":0}', 100),
-(11, 'Star Lord', '1709761300460.png', '{\"x\":1,\"y\":1,\"z\":1}', 'models/plane1/skin.glb', '{\"x\":0,\"y\":1,\"z\":0}', 0);
+(11, 'Star Lord', '1709761300460.png', '{\"x\":1,\"y\":1,\"z\":1}', 'models/plane1/skin.glb', '{\"x\":0,\"y\":1,\"z\":0}', 1000),
+(12, 'badr', '1709761300460.png', '{\"x\":1,\"y\":1,\"z\":1}', 'models/plane1/skin.glb', '{\"x\":0,\"y\":1,\"z\":0}', 800),
+(13, 'abdo1', '1709761300460.png', '{\"x\":1,\"y\":1,\"z\":1}', 'models/plane1/skin.glb', '{\"x\":0,\"y\":1,\"z\":0}', 600),
+(14, 'abdo2', '1709761300460.png', '{\"x\":1,\"y\":1,\"z\":1}', 'models/plane1/skin.glb', '{\"x\":0,\"y\":1,\"z\":0}', 700);
 
 -- --------------------------------------------------------
 
@@ -60,15 +63,15 @@ CREATE TABLE `users` (
   `token` varchar(255) NOT NULL,
   `coins` int(11) NOT NULL DEFAULT 200,
   `xp` int(11) NOT NULL DEFAULT 0
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `token`, `coins`, `xp`) VALUES
-(1, 'ahmed', 'ahmed@email.com', '$2b$10$.QG4yGze9ffc2T/Ccs8cxOJNl6YIUXgRPJfNSk1OJ1SzJrcrUNBtK', 'f0348b7e23dfd834531bfc873162cda8', 500, 0),
-(2, 'ebraheem', 'ebraheem@gmail.com', '$2b$10$5CEyoct3FyQ5wHre86baBOIrkuYopQq6M.CWwhzaZCwr1uzQI8fH.', 'ed270d64616d3779e002dbc2e77db04d', 200, 0);
+(1, 'ahmed', 'ahmed@gmail.com', '$2b$10$.QG4yGze9ffc2T/Ccs8cxOJNl6YIUXgRPJfNSk1OJ1SzJrcrUNBtK', 'f0348b7e23dfd834531bfc873162cda8', 1400, 0),
+(2, 'ebraheem', 'ebraheem@gmail.com', '$2b$10$5CEyoct3FyQ5wHre86baBOIrkuYopQq6M.CWwhzaZCwr1uzQI8fH.', 'ed270d64616d3779e002dbc2e77db04d', 1000, 0);
 
 -- --------------------------------------------------------
 
@@ -79,7 +82,7 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`, `token`, `coins`, `xp`) 
 CREATE TABLE `userskins` (
   `userId` int(11) NOT NULL,
   `skinId` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `userskins`
@@ -88,7 +91,9 @@ CREATE TABLE `userskins` (
 INSERT INTO `userskins` (`userId`, `skinId`) VALUES
 (1, 9),
 (1, 10),
-(1, 11);
+(1, 12),
+(1, 13),
+(2, 9);
 
 --
 -- Indexes for dumped tables
@@ -121,7 +126,7 @@ ALTER TABLE `userskins`
 -- AUTO_INCREMENT for table `skins`
 --
 ALTER TABLE `skins`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
